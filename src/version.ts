e34e548dd16905d2976567edbb9a0eacb7d5745d
Aleksey Efimov <5344652+alksef@users.noreply.github.com>
2026-08-03 15:13:43 +0500

    chore: prepare v0.18.0 release

--- src/version.ts
+++ src/version.ts
@@ -1,3 +1,3 @@
-export const APP_VERSION = '0.17.0';
-export const APP_VERSION_BASE = '0.17.0';
+export const APP_VERSION = '0.18.0';
+export const APP_VERSION_BASE = '0.18.0';
 export const APP_COMMIT_SHA = 'unknown';
