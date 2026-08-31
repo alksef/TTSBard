fc5bf3b3fb2df072c124594f8936e521c633b08c
Aleksey Efimov <5344652+alksef@users.noreply.github.com>
2026-09-01 00:39:35 +0500

    chore(release): prepare 0.25.0

--- src/version.ts
+++ src/version.ts
@@ -1,3 +1,3 @@
-export const APP_VERSION = '0.24.0';
-export const APP_VERSION_BASE = '0.24.0';
+export const APP_VERSION = '0.25.0';
+export const APP_VERSION_BASE = '0.25.0';
 export const APP_COMMIT_SHA = 'unknown';
